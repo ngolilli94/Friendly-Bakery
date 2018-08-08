25d45215cc77dab0661ade851679803e02f1f39e
Lillian N <40573446+ngolilli94@users.noreply.github.com>
2018-08-07 20:06:36 -0400

    Add files via upload

---
+++ baked_goodies.rb
@@ -0,0 +1,33 @@
+class CrepeCakes
+    attr_accessor :photo, :name, :description, :price
+
+    def initialize(photo, name, description, price)
+        @photo = photo 
+        @name = name
+        @description = description
+        @price = price
+    end
+end
+
+class Macarons
+    attr_accessor :photo, :name, :description, :price
+
+    def initialize(photo, name, description, price)
+        @photo = photo 
+        @name = name
+        @description = description
+        @price = price
+    end
+end
+
+class CreamPuffs
+    attr_accessor :photo, :name, :description, :price
+
+    def initialize(photo, name, description, price)
+        @photo = photo 
+        @name = name
+        @description = description
+        @price = price
+    end
+end
+
